d98219ee2cd9672a9b71380b166fe726e816566d
chen650 <chen650@usc.edu>
2019-01-29 18:12:51 -0800

    first commit

---
+++ disease_classification.py
@@ -0,0 +1,54 @@
+"""
+to-do:
+* what to do w/ healthy patients?
+"""
+
+import pandas as pd
+from fuzzyset import FuzzySet
+
+df = pd.read_excel("2018_1Q.xlsx",sheet_name="SearchResults");
+icd = pd.read_excel("icd10conversion.xlsx",sheet_name="Sheet1");
+
+# #### test the above with smaller amt of data for speed
+# df = df[0:9]
+# ####
+
+# fuzzy match trial conditions to ICD descriptions
+conds = df['Conditions'];
+corpus = list(icd['Description']);
+codes = icd['Code'];
+categories = icd['Category'];
+fs = FuzzySet(corpus);
+res = list();
+for val in conds:
+  if pd.isnull(val):
+    resArr = '';
+  else:
+    condArr = val.split("|"); # there can be multiple conditions in a trial
+    resArr = list();
+    for c in condArr:
+      resArr.append(fs.get(c)[0][1]);
+  res.append(resArr);
+
+# translate ICD descriptions to corresponding second-level ICD category
+catList = list(); 
+for resVal in res: # each trial
+  if resVal == '':
+    catStr = "";
+  else:
+    catStr = "";
+    for descr in resVal: # each translated condition within a trial
+      cat = "";
+      for i, r in enumerate(corpus): # check each row in icd corpus
+        if(descr == r): # if the translated condition is of this row in icd corpus, find category
+          cat = categories[i];
+          catStr = "|".join([catStr,cat]);
+          break;
+  catList.append(catStr);
+
+# add column of chapters to trials dataframe
+catList = pd.Series(catList);
+df['ICD10_Categories'] = catList.values;
+
+# write trial data with column of chapters to csv
+df.to_excel('trials_classified2018.xlsx');
